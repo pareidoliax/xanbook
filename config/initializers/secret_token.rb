35a171e7b34b7a542e552a77922e1e43a36b09f9
Xander Miller <xander.miller@gmail.com>
2014-01-17 13:40:22 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xanbook::Application.config.secret_token = 'fb1b3b626c641f0d285eb2a014313e8d7da5e95ed89d65c849adb1b633a458ec9b75ed1af71d179b2285c886cabc7df2e2ec21ca7257ef5caf857de82dd89712'
