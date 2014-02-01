8c3523a447b4c36dfd28f20e75942361d718b296
Xander Miller <xander.miller@gmail.com>
2014-02-01 13:46:05 -0500

    Complete Stage 6 Designing URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Xanbook::Application.routes.draw do
 
-  get "basic/home"
+  get 'home', to: 'basic#home', as: :home
 
-  get "basic/about"
+  get 'about', to: 'basic#about', as: :about
 
-  get "basic/welcome"
+  get 'welcome', to: 'basic#welcome', as: :welcome
 
   devise_for :users
 
-  root to: 'statuses#index'
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'sign_in', to: 'devise/sessions#new', as: :sign_in
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :sign_out
+  end
+
+  root to: 'basic#home'
 
   resources :statuses
 
