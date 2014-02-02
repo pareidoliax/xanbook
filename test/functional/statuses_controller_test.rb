f201fce285af61bebcdac067fa40bca361c97a0c
Xander Miller <xander.miller@gmail.com>
2014-02-02 17:09:06 -0500

    Wrote my own functional tests like a boss.

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class StatusesControllerTest < ActionController::TestCase
   setup do
     @status = statuses(:one)
+    @user = users(:alisa)
   end
 
   test "should get index" do
@@ -23,7 +24,14 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create status" do
+  test 'should be logged in to post a status' do
+    post :create, status: { content: 'Hello' }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:alisa)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -36,14 +44,28 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: @status
-    assert_response :success
+  test "should be redirected from update when not signed in" do
+    put :update, id: @status, status: { content: 'Hello' }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
   end
 
   test "should update status" do
+    sign_in @user
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should redirect from edit when not signed in" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should get edit when signed in" do
+    sign_in @user
+    get :edit, id: @status
+    assert_response :success
   end
 
   test "should destroy status" do
