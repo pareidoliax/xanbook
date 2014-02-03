976204a534d450531ad80a731786deefa7dcf707
Xander Miller <xander.miller@gmail.com>
2014-02-03 16:17:17 -0500

    Added Unit Tests and validations for Status and User.

--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "A status requires content." do
+    status = Status.new
+    assert !status.save
+    assert !status.errors[:content].empty?
+    assert status.errors[:content].include?("can't be blank")
+  end
+
+  test 'A status contents must be longer than 2 characters' do
+    status = Status.new
+    status.content = "u2"
+    assert !status.save
+    assert !status.errors[:content].empty?
+    assert status.errors[:content].include?('is too short (minimum is 3 characters)')
+  end
+
+  test 'A status must belong to a user' do
+    status = Status.new(content: 'Howdy!')
+    assert !status.save
+    assert !status.errors[:user_id].empty?
+    puts status.errors[:user_id].include?("can't be blank")
+  end
+
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -28,11 +28,23 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "a user's profile name without spaces" do
-    user = User.new
+    user = User.new(first_name: "Riho", last_name: "Nishimura", email: 'wife@example.com')
+    user.password  = user.password_confirmation = 'helloworld'
     user.profile_name = "My Spaced Out Profile Name"
 
     assert !user.save
     assert !user.errors[:profile_name].empty?
     assert user.errors[:profile_name].include?("Must be formatted correctly.")
   end
+
+  test "a user can have a valid profile_name" do
+    user = User.new(first_name: "Riho", last_name: "Nishimura", email: 'wife@example.com')
+    user.password  = user.password_confirmation = 'helloworld'
+    user.profile_name = "userAwesome"
+
+    assert user.valid?
+    assert user.save
+    assert user.errors[:profile_name].empty?
+  end
+
 end
